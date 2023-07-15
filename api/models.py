07630fecb81d622b2bbac01e155e60087fee1dcd
Imrul Anwar <imrulanwar836@gmail.com>
2023-07-15 16:45:07 +0600

    created company model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    description = models.TextField()
+    type = models.CharField(max_length=100, choices=(('IT', 'IT'), ('non IT', 'non IT')))
+    created_at = models.DateTimeField(auto_now = True)
+    is_active = models.BooleanField(default=True)
